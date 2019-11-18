bd883912034ef37e7badd28862a4ac9804dd1e71
Guillermo De Fermin <gdefermin@gmail.com>
2019-11-18 10:01:09 +0100

    Removed inline functions to fix compile errors

--- source/mem_allocate.h
+++ source/mem_allocate.h
@@ -3,19 +3,30 @@
 
 #include <malloc.h>
 
-extern __inline__ void* mem_alloc (size_t size) {
+#if (defined(INCLUDE_INLINE_FUNCS) || !defined(NO_INLINE_FUNCS))
+#ifdef INCLUDE_INLINE_FUNCS
+#define _INLINE_ extern
+#else
+#ifdef __GNUC__
+#define _INLINE_ extern __inline__
+#else				/* For Watcom C */
+#define _INLINE_ extern inline
+#endif /* __GNUC__ */
+#endif /* INCLUDE_INLINE_FUNCS */
+
+_INLINE_ void* mem_alloc (size_t size) {
     return malloc(size);
 }
 
-extern __inline__ void* mem_calloc (size_t count, size_t size) {
+_INLINE_ void* mem_calloc (size_t count, size_t size) {
     return calloc(count, size);
 }
 
-extern __inline__ void* mem_realloc (void *p, size_t size) {
+_INLINE_ void* mem_realloc (void *p, size_t size) {
     return realloc(p, size);
 }
 
-extern __inline__ void* mem_align (size_t a, size_t size) {
+_INLINE_ void* mem_align (size_t a, size_t size) {
     #ifdef __wii__
     return memalign(a, size);
     #else
@@ -23,8 +34,10 @@ extern __inline__ void* mem_align (size_t a, size_t size) {
     #endif
 }
 
-extern __inline__ void mem_free (void* mem) {
+_INLINE_ void mem_free (void* mem) {
     free(mem);
 }
+
+#endif /* (defined(INCLUDE_INLINE_FUNCS) || !defined(NO_INLINE_FUNCS)) */
 
 #endif /* _MEM_ALLOCATE_H */
